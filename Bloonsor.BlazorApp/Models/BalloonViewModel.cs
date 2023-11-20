bb356721c0ff8ea310aa20b545b0060638f417e0
QenSel <125572813+QenSel@users.noreply.github.com>
2023-11-20 19:02:44 +0100

    Initial commit

---
+++ Bloonsor.BlazorApp/Models/BalloonViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloonsor.BlazorApp.Models
+{
+    public class BalloonViewModel
+    {
+        public int PositionTop { get; set; }
+        public int PositionLeft { get; set; }
+    }
+}
